4d5cb787e5992b5f6e4d7d1453a78d7696aa1977
JuliaBudavicius <julia.rodrigues@bandtec.com.br>
2022-03-02 13:56:33 -0300

    mais exercicio

--- Prova-Yoshi/estudo-prova-yoshi/src/main/java/sptech/com/br/estudocontinuada/controle/AlunoControler.java
+++ Prova-Yoshi/estudo-prova-yoshi/src/main/java/sptech/com/br/estudocontinuada/controle/AlunoControler.java
@@ -1,9 +1,6 @@
 package sptech.com.br.estudocontinuada.controle;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sptech.com.br.estudocontinuada.entidade.Aluno;
 import sptech.com.br.estudocontinuada.entidade.Linha;
 
@@ -14,11 +11,45 @@ import java.util.List;
 @RequestMapping("/alunos")
 public class AlunoControler {
 
-    //USAR NO JASON
+    //USAR NO POSTMAN
     private List<Aluno> alunos = new ArrayList<>();
 
-    @GetMapping("/{indice}")
-    public List<Aluno> getAlunos(){
+    //Mostrar a lista
+    @GetMapping
+    public List<Aluno> getAlunos() {
         return alunos;
     }
+
+    //Post = Adicionar
+    //Para add no PostMan é (raw) e depois escolher JASON
+    @PostMapping
+    public String adicionarAluno (@RequestBody Aluno novoAluno){
+        alunos.add(novoAluno);
+        return "Aluno cadastrado!!!";
+    }
+
+    //Get = Pegar
+    //Pegar pelo indice
+    @GetMapping("/{indice}")
+    public Aluno getAlunoPorIndice(@PathVariable int indice){
+        return alunos.get(indice);
+    }
+
+    //Put = Atualizar
+    @PutMapping("/{indice}")
+    public String atualizarAlunoPorIndice(
+            @PathVariable int indice,
+            @RequestBody Aluno alunoAtualizado
+    ){
+        alunos.set(indice, alunoAtualizado);
+        return "Aluno atualizado com sucesso";
+    }
+
+    //Delete = Deletar
+    @DeleteMapping("/{indice}")
+    public String removerPorIndice(@PathVariable int indice){
+        alunos.remove(indice);
+        return "Aluno removido";
+    }
 }
+
